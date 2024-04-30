9b97aa5dcac18bcc111472abcd74f06417dd1599
khalil72 <khalilahmed.property051@gmail.com>
2024-04-30 16:48:46 +0500

    import tempelate

---
+++ src/component/Header/Menu.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <header className="masthead">
+    <div className="container">
+        <div className="masthead-subheading">Welcome To Our Studio!</div>
+        <div className="masthead-heading text-uppercase">Its Nice To Meet You</div>
+        <a className="btn btn-primary btn-xl text-uppercase" >Tell Me More</a>
+    </div>
+    </header>
+    
+  )
+}
+
+export default Menu
---
+++ src/component/Home/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Services from './Services'
+import Portfolio from './Portfolio'
+import About from './About'
+import Team from './Team'
+import Clients from './Clients'
+import Contacts from './Contact'
+import Modal from './Modal'
+
+const Home = () => {
+  return (
+    <>
+    <Services />
+    <Portfolio />
+    <About />
+    <Team />
+    <Clients />
+    <Contacts />
+    <Modal />
+    </>
+  )
+}
+
+export default Home
